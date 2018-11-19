c16e8428395e18290da8814f34f7dc7141ea237f
Connor_Rowe <conro1108@gmail.com>
2018-11-19 10:55:02 -0500

    Updates

---
+++ precache-manifest.7f39b7e7d09399a890250d4df576591b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "943caf98d9d5fee1ebcb",
+    "url": "/TTDeploy/static/js/runtime~main.943caf98.js"
+  },
+  {
+    "revision": "1776e0272b28a0f6867d",
+    "url": "/TTDeploy/static/js/main.1776e027.chunk.js"
+  },
+  {
+    "revision": "d903695407de253f29c8",
+    "url": "/TTDeploy/static/js/1.d9036954.chunk.js"
+  },
+  {
+    "revision": "1776e0272b28a0f6867d",
+    "url": "/TTDeploy/static/css/main.dbd38cd0.chunk.css"
+  },
+  {
+    "revision": "b47ad91d13ffec4567ccf0f37eada1b3",
+    "url": "/TTDeploy/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/TTDeploy/precache-manifest.11c25aa1ab93230669dfabf73474d599.js"
+  "/TTDeploy/precache-manifest.7f39b7e7d09399a890250d4df576591b.js"
 );
 
 workbox.clientsClaim();
