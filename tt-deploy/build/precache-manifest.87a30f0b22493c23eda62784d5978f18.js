b05b1481d3ef1e4d378f4f0c24d505d8154ab11e
peakay <peakaylopolis@gmail.com>
2018-11-20 01:20:43 -0500

    try to get rid of fucked up cookie parsing

--- tt-deploy/build/precache-manifest.87a30f0b22493c23eda62784d5978f18.js
+++ tt-deploy/build/precache-manifest.87a30f0b22493c23eda62784d5978f18.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.229c360f.js"
   },
   {
-    "revision": "3f724a78e4a22ed4481e",
-    "url": "/static/js/main.3f724a78.chunk.js"
+    "revision": "e74b142e040340a1a1ba",
+    "url": "/static/js/main.e74b142e.chunk.js"
   },
   {
     "revision": "d3d4517e0fbdeabe1693",
     "url": "/static/js/1.d3d4517e.chunk.js"
   },
   {
-    "revision": "3f724a78e4a22ed4481e",
+    "revision": "e74b142e040340a1a1ba",
     "url": "/static/css/main.77b262f2.chunk.css"
   },
   {
-    "revision": "e0134682d630770b601aacbfe533ab02",
+    "revision": "72f816e49f6b2c6af201534d85e7795b",
     "url": "/index.html"
   }
 ];
--- tt-deploy/build/service-worker.js
+++ tt-deploy/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f8eb7160006d6a1ab3250890811010b.js"
+  "/precache-manifest.87a30f0b22493c23eda62784d5978f18.js"
 );
 
 workbox.clientsClaim();
