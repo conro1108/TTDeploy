faaffb3bbd84fe3300e468007f6fd932aae6442c
tlalka <tlalka@live.unc.edu>
2018-11-22 17:19:23 -0500

    small style changes

--- tt-deploy/src/Body.js
+++ tt-deploy/src/Body.js
@@ -41,13 +41,13 @@ class Tweet extends React.Component{
     render(){
         return(
         <div>
-        <textarea type="text2" id="tweet" name="tweet" onChange={this.handleChange} value={this.state.value} maxLength = {280}/>
+        <textarea type="text2" id="tweet" name="tweet" onChange={this.handleChange} value={this.state.value}/>
         <div className = "sub-body2">
         <div className="b-4">
         Add picture
         </div>
             <div className="b-5">
-    Characters Left: {this.state.value.length}/280
+    {this.state.value.length}/280 
         </div>
         </div> 
         </div>
