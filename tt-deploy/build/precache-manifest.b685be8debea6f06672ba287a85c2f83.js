75faf53c91000e0a2d0b3f2177a2b0b08d5ded0d
tlalka <tlalka@live.unc.edu>
2018-12-01 12:08:31 -0500

    build

---
+++ tt-deploy/build/precache-manifest.b685be8debea6f06672ba287a85c2f83.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "6ed5019e46aebddb8a9f6319600dcfba",
+    "url": "/static/media/logo2.6ed5019e.JPG"
+  },
+  {
+    "revision": "56a41b55b64412fae813a79eb3e37ca5",
+    "url": "/static/media/logo.56a41b55.JPG"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "358e28a8a05b25fa13eb",
+    "url": "/static/js/main.358e28a8.chunk.js"
+  },
+  {
+    "revision": "d1277a5211da08208c90",
+    "url": "/static/js/1.d1277a52.chunk.js"
+  },
+  {
+    "revision": "358e28a8a05b25fa13eb",
+    "url": "/static/css/main.1e4a9300.chunk.css"
+  },
+  {
+    "revision": "5a28c93eb963894cbe6ae4ba95935d1f",
+    "url": "/index.html"
+  }
+];
